a1804848252fae8ee2bbe9527e4e4defb5c5ec9e
KhaiHoan <phamkhaihoan12005@gmail.com>
2025-11-22 00:47:19 +0700

    none

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import { useState } from 'react'
-import { Button } from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='text-red-500'>demo</p>
-      <Button>abc</Button>
     </>
   )
 }

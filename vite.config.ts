3a4fcb163e377d7fea0d2f79d094c070f8999b3d
KhaiHoan <phamkhaihoan12005@gmail.com>
2025-11-22 02:25:37 +0700

    deloy_vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path"
+import path from "path";
 
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,5 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-
+  build: {
+    outDir: "dist", 
+  }
 });
